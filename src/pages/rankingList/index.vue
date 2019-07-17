4b11175a8eca42e5b45c434161e71f03c2d3b1cb
xxx <email@example.com>
2019-07-17 09:20:18 +0800

    hjp

--- src/pages/rankingList/index.vue
+++ src/pages/rankingList/index.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="main-wrap">
     <debug_item path="pageName" v-model="pageName" text="页面名称" />
-    <van-button plain type="primary" size="small">参赛次数</van-button>
-    <van-button plain type="primary" size="small">成绩排名</van-button>
-    <van-button plain type="primary" size="small">鸟王排名</van-button>
-    <van-button plain type="primary" size="small">积分排名</van-button>
+    <rankingListcomponent></rankingListcomponent>
     <debug_item path="matchTimeRanklist" v-model="matchTimeRanklist" text="比赛次数排行列表" />
     <mytabbar></mytabbar>
+    
   </div>
 </template>
 <script>
 /* eslint-disable */
-import mytabbar from '@/components/mytabbar/mytabbar'
-import debug_item from '@/components/common/debug_item/debug_item'
+import mytabbar from "@/components/mytabbar/mytabbar";
+import debug_item from "@/components/common/debug_item/debug_item";
+import rankingListcomponent from "./rankingListComponent";
 import util from "@/utils/util";
 
 export default {
   components: {
     mytabbar,
-    debug_item
+    debug_item,
+    rankingListcomponent
   },
   data() {
     return {
@@ -29,29 +29,24 @@ export default {
 
   methods: {
     onShow() {
-
       console.log("rankingList-mpvue.data", this);
       // mpvue.setData({show: true})
     }
-
   },
   async created() {
     console.log("唐球达人-created");
-
-
   },
   async mounted() {
-  
     /**
      * ajax请求参赛次数排行榜
      * 请求会员表tangball_member
      */
     let { data } = await util.post({
-      url: global.PUB.domain + '/crossList?page=tangball_member',
+      url: global.PUB.domain + "/crossList?page=tangball_member",
       param: {
         pageSize: 50, //每页50条数据
-        sortJson: { entries: 1 },//按参赛次数降序排序
-         selectJson: { P1: 1 ,entries: 1 ,name: 1},//指定返回哪些字段
+        sortJson: { entries: 1 }, //按参赛次数降序排序
+        selectJson: { P1: 1, entries: 1, name: 1 } //指定返回哪些字段
       }
     });
     this.matchTimeRanklist = data.list;
---
+++ src/pages/rankingList/rankingListComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="button-center">
+      <van-button plain type="primary" size="small">参赛次数</van-button>
+      <van-button plain type="primary" size="small">成绩排名</van-button>
+      <van-button plain type="primary" size="small">鸟王排名</van-button>
+      <van-button plain type="primary" size="small">积分排名</van-button>
+    </div>
+
+    <van-tabs :active="active">
+      <van-tab title="男子">
+        <div class="ranking-title">
+          <div>排名</div>
+          <div>男子选手</div>
+          <div>成绩</div>
+        </div>
+
+        <div class="ranking-content">
+          <div>1</div>
+          <div>
+            <a>张三</a>
+            <a>地区</a>
+          </div>
+          <div>
+            <a>99</a>
+            <a>2019-10-1</a>
+          </div>
+        </div>
+      </van-tab>
+
+      <van-tab title="女子">
+        <div class="ranking-title">
+          <div>排名</div>
+          <div>女子选手</div>
+          <div>成绩</div>
+        </div>
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.button-center {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 800px;
+  text-align: center;
+}
+.button-center van-button {
+  margin: 5px;
+}
+.ranking-title {
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+}
+.ranking-title div {
+  width: 100px;
+  height: 30px;
+  line-height: 30px;
+  display: inline-block;
+  text-align: center;
+  margin: 10px 5px;
+}
+
+.ranking-content {
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+  background-color: aquamarine;
+}
+.ranking-content div {
+  background-color: antiquewhite;
+  width: 100px;
+  display: inline-block;
+  text-align: center;
+  margin: 10px 5px;
+}
+.ranking-content div:first-child {
+  background-color: antiquewhite;
+  width: 100px;
+  height: 40px;
+  line-height: 40px;
+  display: inline-block;
+  text-align: center;
+  margin: 10px 5px;
+}
+/* .ranking-content div a {
+  background-color: red;
+  height: 25px;
+  line-height: 25px;
+  display: inline-block;
+  text-align: center;
+  margin: 10px 5px;
+} */
+</style>
