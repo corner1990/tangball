65d0108e5bad15378a259d3f9953ad42cd412f9f
lyh <113381161@qq.com>
2019-07-25 10:19:36 +0800

    lyh

--- src/pages/articleDetail/index.vue
+++ src/pages/articleDetail/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="main-wrap">
-    <div class="" >123</div>
       <web-view :src="url"></web-view>
-    <div class="" >456</div>
-    <mytabbar></mytabbar>
   </div>
 </template>
 <script>
@@ -22,7 +19,7 @@ export default {
   },
   data() {
     return {
-      url: "https://mp.weixin.qq.com/",
+      url: "https://mp.weixin.qq.com/s?__biz=MjM5OTU0MzkwNA==&mid=2648840002&idx=1&sn=408f8b7cd8e79fd7833ca602749b4c54&chksm=bf2f624d8858eb5bb8ecdf86f0801c6f79728a60ed2ef7328ba2f74454979d5075767e68a482&mpshare=1&scene=1&srcid=&sharer_sharetime=1563965861424&sharer_shareid=99cae6b5001af0c60a90266f7fc09247&key=a1ae7233401db86daaae614069516d8a50172d715cab918841951c0851aa447c4cb13e6daa1666179989416a446cda0b1b91668b4bd3b1c0e7e77e77a901b07caca9c2b05e3afb2bc0b4354e8b48fbbc&ascene=1&uin=NjU5MzgxMjQw&devicetype=Windows+10&version=62060833&lang=zh_CN&pass_ticket=T72dYdOdKZmtyzs0WspoZIa5tDM8HYQWZnKO3sMoCFfiqp28qVrFntA6HBQUKdGu",
       myMsgList: null,
      
     };
