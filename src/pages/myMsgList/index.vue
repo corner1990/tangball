416bfdcfcdd248a5c998f193eff7c94b8ce424ca
LXC <email@example.com>
2019-08-07 15:34:25 +0800

    00

--- src/pages/myMsgList/index.vue
+++ src/pages/myMsgList/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="main-wrap">
     <mytabbar></mytabbar>
-    <div class="top-box">
+    <div>
+      <van-tabs :active="active" @change="onChange">
+        <van-tab title="未读"></van-tab>
+        <van-tab title="已读"></van-tab>
+      </van-tabs>
+    </div>
+    <!-- <div class="top-box">
       <div class="read-box" :class="!focuss?'masEvolve':''" @click="read">已读</div>
       <div class="unread-box" :class="focuss?'masEvolve':''" @click="unread">未读</div>
-    </div>
+    </div>-->
     <msg :transformm="transform" :crowArr="msgg" @newMsgg="moveMsg" @spliceMsgg="closeDoor"></msg>
   </div>
 </template>
@@ -22,32 +28,43 @@ export default {
   },
   data() {
     return {
-      transform: true,//传递向子组件告知已读未读的状态值
-      msgId: null,//传递给接口的消息id
-      myMsgList: null,//传递接口的消息列表
-      myMsgRead: null,//传递接口的已读消息列表
-      dictMsgRead: null,//传递接口的已读消息数字字典
-      msgg0: [],//未读数组
-      msgg: [],//传递向子组件的数组值
-      focuss: true,//已读未读聚焦
-      evolve: "masEvolve",//按钮聚焦样式
-      crow2: [],//已读数组
-      pageName: "个人中心-系统消息列表"//页面名
+      active: 0,//默认聚焦未读
+      transform: true, //传递向子组件告知已读未读的状态值
+      msgId: null, //传递给接口的消息id
+      myMsgList: null, //传递接口的消息列表
+      myMsgRead: null, //传递接口的已读消息列表
+      dictMsgRead: null, //传递接口的已读消息数字字典
+      msgg0: [], //未读数组
+      msgg: [], //传递向子组件的数组值
+      focuss: true, //已读未读聚焦
+      evolve: "masEvolve", //按钮聚焦样式
+      crow2: [], //已读数组
+      pageName: "个人中心-系统消息列表" //页面名
     };
   },
   computed: {
-  //唐球会员信息-在vuex中获取
-    tangballUserInfo:function() {
-      return this.$store.state.tangballUserInfo
+    //唐球会员信息-在vuex中获取
+    tangballUserInfo: function() {
+      return this.$store.state.tangballUserInfo;
     }
   },
   mounted() {
     //在创建后调用一次消息列表接口接口
     // 将数据分别加载至页面假数组中，其中在未读时点击会向已读接口传递数据
-    this.getMyMsgList(); 
+    this.getMyMsgList();
     console.log(this.tangballUserInfo.P1);
   },
+
   methods: {
+    //顶部聚焦按钮
+    onChange(event) {
+      console.log("event", event);
+      if (event.mp.detail.index == 0) {
+        this.unread();
+      } else {
+        this.read();
+      }
+    },
     // 已读按钮
     // 此处控制按钮focuss聚焦，并且传递向子组件的值
     // msgg值控制子组件显示的为哪个数组
@@ -63,14 +80,17 @@ export default {
       this.msgg = this.msgg0;
       this.transform = true;
     },
-// 被子组件$emit触发的方法1，将子组件被点击的未读消息的id传递到父组件
-// 触发已读消息接口，传递已读的消息id
-// 并且修改页面未读消息数组，将页面被点击的已读移动至未读（此处只修改未读，已读在子组件修改）
+    // 被子组件$emit触发的方法1，将子组件被点击的未读消息的id传递到父组件
+    // 触发已读消息接口，传递已读的消息id
+    // 并且修改页面未读消息数组，将页面被点击的已读移动至未读（此处只修改未读，已读在子组件修改）
     moveMsg(transmit) {
       this.msgId = transmit.newcrow.msgId;
       this.crow2.push(transmit.newcrow.unreadBox);
       if (this.transform) {
-        this.setReadStatus({ memberId: this.tangballUserInfo.P1, msgId: this.msgId });
+        this.setReadStatus({
+          memberId: this.tangballUserInfo.P1,
+          msgId: this.msgId
+        });
       }
     },
     // 被子组件$emit触发的方法2，解决用户未点击消息确定就切换已读未读按钮，导致消息数组无法正确删除的问题
@@ -84,7 +104,7 @@ export default {
         this.msgg0.splice(this.gant, 1);
       }
     },
-// 这里是消息列表的接口，此接口只在页面创建后加载一次，将数据加载至页面已读未读的假数组中
+    // 这里是消息列表的接口，此接口只在页面创建后加载一次，将数据加载至页面已读未读的假数组中
     async getMyMsgList(_json) {
       let { data } = await util.post({
         //请求接口
@@ -92,7 +112,10 @@ export default {
         param: {
           findJson: {
             //或查询条件：range==1或[range==2&&memberIdList包含当前会员id]
-            $or: [{ range: 1 }, { range: 2, memberIdList: this.tangballUserInfo.P1 }]
+            $or: [
+              { range: 1 },
+              { range: 2, memberIdList: this.tangballUserInfo.P1 }
+            ]
           }
         } //传递参数
       });
@@ -102,12 +125,12 @@ export default {
           url: global.PUB.domain + "/crossList?page=tangball_msg_read",
           param: {
             findJson: {
-              memberId:this.tangballUserInfo.P1
+              memberId: this.tangballUserInfo.P1
             }
           } //传递参数
         });
         this.myMsgRead = data.list;
-        console.log("已读",data);
+        console.log("已读", data);
       }
       // this.dictMsgRead = {}; //消息阅读记录的数据字典对象
       // this.myMsgRead.forEach(msgReadEach => {
@@ -115,7 +138,7 @@ export default {
       //   this.dictMsgRead[msgReadEach.msgId] = msgReadEach;
       // });
       //使用lodash.keyBy制作数据字典
-      console.log("消息",data);
+      console.log("消息", data);
       this.dictMsgRead = this.$lodash.keyBy(this.myMsgRead, "msgId");
       //循环：{消息数组}
       data.list.forEach(msgEach => {
@@ -124,15 +147,15 @@ export default {
         if (docRead) {
           msgEach.isRead = true; //已读
           msgEach.readTime = docRead.readTime;
-          this.crow2.push(msgEach);//加载数据至已读接口
+          this.crow2.push(msgEach); //加载数据至已读接口
         } else {
           msgEach.isRead = false; //未读
-          this.msgg0.push(msgEach);//加载数据至未读接口
+          this.msgg0.push(msgEach); //加载数据至未读接口
         }
       });
       this.myMsgList = data.list;
-      this.msgg = this.msgg0;//页面加载后使子组件默认显示未读数组
-      console.log("消息列表",this.myMsgList);
+      this.msgg = this.msgg0; //页面加载后使子组件默认显示未读数组
+      console.log("消息列表", this.myMsgList);
     },
     /**
      * 函数：{设置消息已读状态的函数}
@@ -160,7 +183,7 @@ export default {
   padding-bottom: 100px;
 }
 .top-box {
-  margin-top: 8px;
+  margin: 8px 0px;
   height: 35px;
   background: white;
   position: relative;
@@ -173,6 +196,7 @@ export default {
   line-height: 35px;
   top: 0;
   right: 0px;
+  color: darkgrey;
 }
 .unread-box {
   width: 50%;
@@ -182,9 +206,10 @@ export default {
   line-height: 35px;
   top: 0;
   left: 0px;
+  color: darkgrey;
 }
 .masEvolve {
-  background-color: #F4B116;
-  color: #FFFFFF;
+  border-bottom: #f4b116 5px solid;
+  color: black;
 }
 </style>
